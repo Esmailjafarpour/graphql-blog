1b0e754c208a8c0a11304390b73659f3b917d30e
nader <esmailjafarpour29@gmail.com>
2023-05-04 15:03:25 +0330

    Preliminary and test taking

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ApolloClient , ApolloProvider , InMemoryCache } from "@apollo/client";
+
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const client = new ApolloClient({
+  uri :"https://api-us-west-2.hygraph.com/v2/cl5ex4xej1o9h01ug1jevguyc/master",
+  cache : new InMemoryCache()
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+  <ApolloProvider client={client}>
+      <App />
+  </ApolloProvider>
+
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
