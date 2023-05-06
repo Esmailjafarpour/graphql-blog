9d5143777c7e8068eb122d8685526bab6a786e00
nader <esmailjafarpour29@gmail.com>
2023-05-06 12:29:45 +0330

    grid Classification of posts

--- src/components/blogs/Blogs.js
+++ src/components/blogs/Blogs.js
@@ -3,6 +3,9 @@ import React from 'react';
 import { useQuery } from "@apollo/client";
 import { GET_BLOGS_INFO } from "../../graphql/queries";
 
+import {Container , Grid , Typography} from "@mui/material";
+
+import CardEl from "../../components/shared/CardEl";
 
 const Blogs = () => {
 
@@ -12,9 +15,13 @@ const Blogs = () => {
      if(errors) return <h4>Errors ...</h4>
      console.log(data)
      return (
-          <div>
-              Blog 
-          </div>
+          <Grid container spacing={2}>
+               {data.posts.map(post => (
+                  <Grid item xs={12} sm={6} md={4} key={post.id}>
+                    <CardEl {...post}/>
+                  </Grid>  
+               ))}
+          </Grid>
      );
 }
 
---
+++ src/components/shared/CardEl.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const CardEl = (props) => {
+     console.log(props.title)
+     return (
+          <div>
+               CardEl
+          </div>
+     );
+}
+
+export default CardEl;
