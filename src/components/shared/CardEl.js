bd97da02a09d3e08c977365d38ba87b9352dce67
nader <esmailjafarpour29@gmail.com>
2023-05-06 13:38:44 +0330

    Making cards related to articles

--- src/components/shared/CardEl.js
+++ src/components/shared/CardEl.js
@@ -1,12 +1,54 @@
-import React from 'react';
+import React from "react";
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardMedia,
+  CardActions,
+  Avatar,
+  Typography,
+  Divider,
+  Button
+} from "@mui/material";
 
-const CardEl = (props) => {
-     console.log(props.title)
-     return (
-          <div>
-               CardEl
-          </div>
-     );
-}
+import {Link} from "react-router-dom"
+
+const CardEl = ({ title, slug, coverPhoto, author }) => {
+  console.log(author.name);
+  return (
+    <div>
+      <Card
+        style={{ boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px", borderRadius: 4 }}
+      >
+        <CardHeader
+          avatar={<Avatar src={author.avatar.url} sx={{ marginLeft: 2 }} />}
+          title={
+            <Typography component="p" variant="p" color="text.secondary">
+              {author.name}
+            </Typography>
+          }
+        />
+        <CardMedia
+          component="img"
+          image={coverPhoto.url}
+          height="194"
+          alt={slug}
+          fontWeight={600}
+        />
+        <CardContent>
+          <Typography component="h3" variant="h6" color="text.primary">
+            {title}
+          </Typography>
+        </CardContent>
+        <Divider variant="middle" sx={{margin : "10px"}}/>
+        <CardActions>
+          {/* <Link to={`/blogs/${slug}`} style={{textDecoration : "none" , width : "100%"}}> */}
+               <Button variant="outlined" size="small" sx={{width : "100%" , borderRadius : 3}}>مطالعه ی مقاله</Button>
+          {/* </Link> */}
+        </CardActions>
+      </Card>
+    </div>
+  );
+};
 
 export default CardEl;
