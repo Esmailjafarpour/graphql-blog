3d7cc49ef8ebe5844dd86c764d146bfa326c969e
nader <esmailjafarpour29@gmail.com>
2023-05-07 19:22:52 +0330

    Show comments related to each post

--- src/components/comment/Comments.js
+++ src/components/comment/Comments.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React from "react";
 import { useQuery } from "@apollo/client";
 import { GET_POST_COMMENTS } from "../../graphql/queries";
+import { Container, Grid, Typography, Avatar, Box } from "@mui/material";
 
+const Comments = ({ slug }) => {
+  const { loading, data, errors } = useQuery(GET_POST_COMMENTS, {
+    variables: { slug },
+  });
 
-const Comments = ({slug}) => {
-    const {loading , data, errors} = useQuery(GET_POST_COMMENTS,{
-     variables : {slug}
-    })
-    console.log(data)
-     return (
-          <div>
-               fdgfdg
-          </div>
-     );
-}
+  if(loading) return null;
+
+  return (
+    <Grid
+      container
+      sx={{
+        boxShadow: "rgba(0,0,0,0.1) 0px 4px 12px",
+        borderRadius: 4,
+        py: 1,
+        mt: 5,
+      }}
+    >
+      <Grid item xs={12} m={2}>
+        <Typography component="p" variant="h6" fontWeight={700} color="primary">
+          کامنت ها
+        </Typography>
+        {data.comments.map((comment) => (
+          <Grid key={comment.id} item xs={12} m={2} p={2} border="1px silver solid" borderRadius={1}>
+               <Box component="div" display="flex" alignItems="center" mb={3}>
+                    <Avatar>{comment.name[0]}</Avatar>
+                    <Typography component="span" variant="p" fontWeight={700} m={1}>{comment.name}</Typography>
+               </Box>
+               <Typography component="p" variant="p">{comment.text}</Typography>
+          </Grid>
+        ))}
+      </Grid>
+    </Grid>
+  );
+};
 
 export default Comments;
